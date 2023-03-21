8482ae6aac6368b5ad4eb7686c0762939d8cda55
mohd-osama-47 <mohd.osama.elnour@gmail.com>
2023-03-21 22:07:51 +0400

    test YOLO's builtin BYTEtrack implementation.

---
+++ polaris/track.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+import cv2
+from ultralytics import YOLO
+
+# Get the path of this file and use it to find the model to load
+cur_path = os.path.dirname(__file__)
+
+# Load a model
+model = YOLO(os.path.join(cur_path, 'model/model_weights.pt'))  # load the trained model
+
+model.track(source="/home/mosama/tii-competition-attempt/output.avi", show=True, verbose=False)
